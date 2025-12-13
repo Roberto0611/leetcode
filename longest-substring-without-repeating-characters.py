216e394df67dff0fa5cd2b8bc44ebdebdf451c46
Roberto0611 <ochoacuevasr@gmail.com>
2025-12-13 11:48:01 -0700

    longest substring...

---
+++ longest-substring-without-repeating-characters.py
@@ -0,0 +1,16 @@
+def lengthOfLongestSubstring(s):
+    charSet = set()
+    l = 0
+    output = 0
+
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l += 1
+        charSet.add(s[r])
+        output = max(output, r - l + 1)
+    return output
+
+# call
+s = "abcabcbb"
+print(lengthOfLongestSubstring(s))
