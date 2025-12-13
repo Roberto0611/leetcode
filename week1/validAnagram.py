68be7ceea5132b212ef8399449454d4c3e312a19
Roberto0611 <ochoacuevasr@gmail.com>
2025-12-13 10:00:16 -0700

    valid Anagram

--- week1/validAnagram.py
+++ week1/validAnagram.py
@@ -1,18 +1,9 @@
 # 242. Valid Anagram
 # Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+from collections import Counter
 
-# approach 1
 def isAnagram(s,t):
-    countS = {}
-    countT = {}
-
-    for letra in s:
-        countS[letra] = countS.get(letra,0) + 1
-        
-    for letra in t:
-        countT[letra] = countT.get(letra,0) + 1
-    
-    return countS == countT
+    return Counter(s) == Counter(t)
 
 # call
 s = 'anagram'
