861370318be83f8dbffd0495325ab2693a783e5a
Roberto0611 <ochoacuevasr@gmail.com>
2025-12-26 16:14:31 -0700

    valid palindrome again

--- week1/validPalindrome.py
+++ week1/validPalindrome.py
@@ -5,22 +5,21 @@
 
 # two pointers
 def isPalindrome(s):
-    l = 0
-    r = len(s) - 1
-    while l < r:
-        if not s[l].isalnum():
-            l+=1
-            continue
-        if not s[r].isalnum():
-            r-=1 
-            continue
-        if s[l].lower() == s[r].lower():
-            l+=1
-            r-=1
-            continue
-        else:
-            return False
-    return True
+        s = s.lower()
+        a = 0
+        b = len(s) - 1
+        while a < b:
+            if not s[a].isalnum():
+                a+=1 
+                continue
+            if not s[b].isalnum():
+                b-=1
+                continue
+            if s[a] != s[b]:
+                return False
+            a+=1
+            b-=1
+        return True
 
 # call
 s = 'Race car'
